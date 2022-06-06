e51d83d974a3788a479089ac1d0ee8030d477851
Denis Borodin <borocopy@gmail.com>
2022-06-06 14:28:57 +0300

    Memory leak found

--- src/Group.cpp
+++ src/Group.cpp
@@ -15,7 +15,6 @@ void Group::decrease_time_span() {
   if (time_left == 0) {
     emit(get_signal_emitter(), Base::Command::GROUP_LEAVE,
          std::to_string(get_uid()));
-		delete this;
   }
 }
 
